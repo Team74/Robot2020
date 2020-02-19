0db7cbb4203f41d8d82844e48444c8c8045f104a
SamV-42 <vanderarks@brpsk12.org>
2020-02-18 19:32:20 -0500

    Inital hood closed loop tuning

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,4 +7,21 @@ public class Constants {
     public static final double kFlywheelI = 0.0;
     public static final double kFlywheelD = 0.0;
 
+
+    public static final int kHoodMinimumHeight = 0;
+    public static final int kHoodMaxHeight = 25000;
+    public static final int kHoodMaxVelocity = 400;
+    public static final int kHoodMaxAcceleration = 1000;
+    public static final double kHoodF = 0.46454545;
+    public static final double kHoodP = 0.0;
+    public static final double kHoodI = 0.0;
+    public static final double kHoodD = 0.0;
+
+    public static final int kTurretMaxVelocity = 0;
+    public static final int kTurretMaxAcceleration = 0;
+    public static final double kTurretF = 0.0;
+    public static final double kTurretP = 0.0;
+    public static final double kTurretI = 0.0;
+    public static final double kTurretD = 0.0;
+
 }
