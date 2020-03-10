ae60011ccab6d312b539ec991fe78703873cfb45
Jacob Harrelson <jacob.harrelson@upandrunningsoftware.com>
2020-03-10 16:38:56 -0400

    Worked on drive code

--- src/main/java/frc/robot/Drivebase.java
+++ src/main/java/frc/robot/Drivebase.java
@@ -25,6 +25,24 @@ public class Drivebase implements Updateable {
     private double autoTurnScalar = 1.0/90.0;
     private double autoTurnDeadband = 0.5;
 
+    private double kThrottleDeadband = 0.2;
+    private double kSteeringNonlinearity = 0.5;
+
+    private final double kNegInertiaThreshold = 0.65;
+    private final double kNegInertiaTurnScalar = 3.5;
+    private final double kNegInertiaCloseScalar = 4.0;
+    private final double kNegInertiaFarScalar = 5.0;
+
+    private final double kSensitivity = 0.65;
+
+    private final double kQuickStopDeadband = 0.5;
+    private final double kQuickStopWeight = 0.1;
+    private final double kQuickStopScalar = 5.0;
+
+    private double oldSteering = 0.0;
+    private double quickStopAccumlator = 0.0;
+    private double negInertiaAccumlator = 0.0;
+
     //The value needed to convert from distance, to neo encoder ticks
     private double distanceConversionFactor = 1.0;
     private double autoDistanceDeadband = 50;
@@ -228,6 +246,82 @@ public class Drivebase implements Updateable {
         DriveCommands.leftMotorOutput = leftMotorOutput;
         DriveCommands.rightMotorOutput = rightMotorOutput;
     }
+
+    public void curvatureDrive(double throttle, double steering, boolean isQuickTurn) {
+        throttle = Utilities.handleDeadband1D(throttle, kThrottleDeadband);
+        steering = Utilities.handleDeadband1D(steering, kSteeringDeadband);
+        double negInertia = steering - oldSteering;
+        oldSteering = steering;
+
+        final double denominator = Math.sin(Math.PI / 2.0 *kSteeringNonlinearity);
+        steering = Math.sin(Math.PI / 2.0 * kSteeringNonlinearity * steering) / denominator;
+        steering = Math.sin(Math.PI / 2.0 * kSteeringNonlinearity * steering) / denominator;
+
+        double linearPWR, angularPWR;
+        double rightPWR, leftPWR, overPWR;
+
+        double negInertiaScalar;
+        if ((steering * negInertia) > 0) {
+            negInertiaScalar = kNegInertiaTurnScalar;
+        } else {
+            if (Math.abs(steering) > kNegInertiaThreshold) {
+                negInertiaScalar = kNegInertiaFarScalar;
+            } else {
+                negInertiaScalar = kNegInertiaCloseScalar;
+            }
+        }
+        double negInertiaPWR = negInertia * negInertiaScalar;
+        negInertiaAccumlator += negInertiaPWR;
+
+        steering = steering + negInertiaAccumlator;
+        if (negInertiaAccumlator > 1) {
+            negInertiaAccumlator -= 1;
+        } else if (negInertiaAccumlator < -1) {
+            negInertiaAccumlator += 1;
+        } else {
+            negInertiaAccumlator = 0;
+        }
+        linearPWR = throttle;
+
+
+        if (isQuickTurn) {
+            if(Math.abs(linearPWR) < kQuickStopDeadband) {
+                quickStopAccumlator = (1 - kQuickStopWeight) * quickStopAccumlator + kQuickStopWeight * Utilities.limit(steering, 1.0) * kQuickStopScalar;
+            }
+            overPWR = 1.0;
+            angularPWR = steering;
+        } else {
+            overPWR = 0.0;
+            angularPWR = Math.abs(linearPWR) * steering * kSensitivity - quickStopAccumlator;
+            if (quickStopAccumlator > 1) {
+                quickStopAccumlator -= 1;
+            } else if (quickStopAccumlator < -1) {
+                quickStopAccumlator += 1;
+            } else {
+                quickStopAccumlator = 0.0;
+            }
+        }
+        rightPWR = leftPWR = linearPWR;
+        leftPWR += angularPWR;
+        rightPWR -= angularPWR;
+
+        if (leftPWR > 1.0) {
+            rightPWR -= overPWR * (leftPWR - 1.0);
+            leftPWR = 1.0;
+        } else if (rightPWR > 1.0) {
+            leftPWR -= overPWR * (rightPWR - 1.0);
+            rightPWR = 1.0;
+        } else if (leftPWR < -1.0) {
+            rightPWR += overPWR * (-1.0 - leftPWR);
+            leftPWR = -1.0;
+        } else if (rightPWR < -1.0) {
+            leftPWR += overPWR * (-1.0 - rightPWR);
+            rightPWR = -1.0;
+        }
+
+        DriveCommands.leftMotorOutput = leftPWR;
+        DriveCommands.rightMotorOutput = rightPWR;
+    }
     
     public void handleShift(ShiftState newState) {
         switch(newState) {
@@ -278,3 +372,10 @@ public class Drivebase implements Updateable {
         Teleop;
     }
 }
+
+private double kSteeringDeadband = 0.2;
+
+
+    public void updateOutputs(double throttle, double steering, boolean isQuickTurn) {
+        if (isCurvatureDrive) {
+            
